d636e0ea1daebe38991b1344d23f7aee146b5f21
Shingirayi Innocent Mandebvu <shingim1@gmail.com>
2020-10-13 23:40:24 +0200

    feat: add contact section

---
+++ src/components/ContactSection.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import '../styles/ContactSection.css';
+
+export const ContactSection = () => (
+  <section id="contact" className="contact">
+    <div className="container">
+
+      <div className="section-title">
+        <h2>Contact</h2>
+        <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+      </div>
+
+      <div className="row" data-aos="fade-in">
+
+        <div className="col-lg-5 d-flex align-items-stretch">
+          <div className="info">
+            <div className="address">
+              <i className="icofont-google-map" />
+              <h4>Location:</h4>
+              <p>4 & 6 Duxford Rd, Greencroft, ZW</p>
+            </div>
+
+            <div className="email">
+              <i className="icofont-envelope" />
+              <h4>Email:</h4>
+              <p>simandebvu@icloud.com</p>
+            </div>
+
+            <div className="phone">
+              <i className="icofont-phone" />
+              <h4>Call:</h4>
+              <p>+263 772 770 994</p>
+            </div>
+
+            <iframe src="https://maps.google.com/maps?q=greencroft%20zimbabwe&t=&z=15&ie=UTF8&iwloc=&output=embed" frameBorder="0" style={{ border: '0', width: '100%', height: '290px' }} title="my location" allowFullScreen />
+          </div>
+
+        </div>
+
+        <div className="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
+          <form action="forms/contact.php" method="post" role="form" className="php-email-form">
+            <div className="form-row">
+              <div className="form-group col-md-6">
+                <label htmlFor="name">Your Name</label>
+                <input type="text" name="name" className="form-control" id="name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                <div className="validate" />
+              </div>
+              <div className="form-group col-md-6">
+                <label htmlFor="name">Your Email</label>
+                <input type="email" className="form-control" name="email" id="email" data-rule="email" data-msg="Please enter a valid email" />
+                <div className="validate" />
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="name">Subject</label>
+              <input type="text" className="form-control" name="subject" id="subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+              <div className="validate" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="name">Message</label>
+              <textarea className="form-control" name="message" rows="10" data-rule="required" data-msg="Please write something for us" />
+              <div className="validate" />
+            </div>
+            <div className="mb-3">
+              <div className="loading">Loading</div>
+              <div className="error-message" />
+              <div className="sent-message">Your message has been sent. Thank you!</div>
+            </div>
+            <div className="text-center"><button type="submit">Send Message</button></div>
+          </form>
+        </div>
+
+      </div>
+
+    </div>
+  </section>
+);
+
+export default ContactSection;
