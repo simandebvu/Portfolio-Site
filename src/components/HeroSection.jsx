8f8b58b8f24bfcfbd957fbd6e77286077d310381
Shingirayi Innocent Mandebvu <shingim1@gmail.com>
2020-10-13 23:40:11 +0200

    feat: add hero section

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/HeroSection.css';
+
+export const HeroSection = () => (
+  <>
+    <section id="hero" className="d-flex flex-column justify-content-center align-items-center">
+      <div className="hero-container" data-aos="fade-in">
+        <h1>Shingirayi Mandebvu</h1>
+        <p>
+          I love to code
+        </p>
+      </div>
+    </section>
+  </>
+);
+
+export default HeroSection;
