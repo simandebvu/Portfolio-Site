cb39ec1cb013a9bd1679b8d58c52a2220421f227
Shingirayi Innocent Mandebvu <shingim1@gmail.com>
2020-10-14 22:51:17 +0200

    feat: add typed text

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../styles/HeroSection.css';
+import Typed from 'typed.js';
 
-export const HeroSection = () => (
-  <>
-    <section id="hero" className="d-flex flex-column justify-content-center align-items-center">
-      <div className="hero-container" data-aos="fade-in">
-        <h1>Shingirayi Mandebvu</h1>
-        <p>
-          I love to code
-        </p>
-      </div>
-    </section>
-  </>
-);
+export const HeroSection = () => {
+  const text = [' create!', ' implement!', ' code!'];
+  useEffect(() => {
+    const options = {
+      strings: text,
+      typeSpeed: 50,
+      backSpeed: 50,
+    };
+    const typed = new Typed('#typedStuff', options);
+
+    return () => {
+      typed.destroy();
+    };
+  }, [text]);
+  return (
+    <>
+      <section id="hero" className="d-flex flex-column justify-content-center align-items-center">
+        <div className="hero-container" data-aos="fade-in">
+          <h1>Shingirayi Mandebvu</h1>
+          <p>
+            I love to
+            {' '}
+            <span id="typedStuff" />
+          </p>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default HeroSection;
