e6e39f66fc35780c5da12f8615219ac11305d862
Shingirayi Innocent Mandebvu <shingim1@gmail.com>
2020-10-16 00:09:25 +0200

    feat: add provider

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import { Provider } from 'react-redux';
 import App from './components/App';
+import store from './store/index';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
